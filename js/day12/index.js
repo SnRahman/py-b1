f5dab4afeaa93a4297ece3dd7d6f9bfe829afa82
SnRahman <shahzadanouman@hotmail.com>
2024-08-19 18:54:47 +0500

    js

---
+++ js/day12/index.js
@@ -0,0 +1,99 @@
+// 1
+// alert(' Hello - external ')
+
+// 2
+document.write('Hello world!!!')
+
+// 3
+console.log('hello world!!!- console')
+
+// data types
+// 1
+// interger
+// 0-9
+
+var a = 20
+console.log(a)
+
+a = 25
+
+console.log(a + 2)
+
+console.log(a)
+
+
+// 2
+// decimal
+// 0.0 - 9.9
+
+var b = 25.36
+
+console.log( typeof b )
+// console.log( b instanceof Float32 )
+
+console.log(b)
+
+
+// 3
+// String
+// '45', "0-9 , 0.0 - 9.9, a-z , A-Z, ))((*^&^%$^%$#%$*&)) "
+
+let c = 'Hello'
+console.log(c)
+
+c = 'Hello World!!'
+console.log(typeof c)
+
+console.log(c)
+
+// 4
+// Boolean
+// 0 1 on Off, true false
+
+// let d = true
+let d = false
+console.log(d)
+
+console.log( typeof d)
+
+
+
+// 5
+// Array
+
+
+let e = [12,56,98,345,2,234,234]
+
+console.log(e)
+console.log( e[3] )
+console.log( e[5] )
+
+let f = [23,98.43,'hello',true,[1,2,3]]
+console.log(f)
+console.log(f[4])
+console.log(f[4][1])
+
+
+
+// assignment operator
+// =
+
+// variable - static
+
+// 1 = 2
+
+
+// let var
+
+
+
+
+
+
+
+
+
+
+
+
+
